8a81dcbc834780bc5eb4999a53b14727164b082f
ennegineer <enne.face@gmail.com>
2023-12-06 20:55:24 -0800

    Day 1 Part 2 complete

--- Day1/main.py
+++ Day1/main.py
@@ -7,14 +7,12 @@ with open("input.txt", "r") as input:
 # Combine the first digit and last digit from each line to get the two-digit input values.
 # Find the total of all the values.
 
-def PartOne(data):
+def PartOne(input_data):
     # Get numbers from string 
-    instructions = []
     sum = 0
-    for row in data:
+    for row in input_data:
         digits = [int(i) for i in row if i.isdigit()]
         instruction = str(digits[0]) + str(digits[-1])
-        instructions.append(instruction)
         sum += int(instruction)
     print(sum)
     return sum
@@ -22,7 +20,30 @@ def PartOne(data):
 
 # Part Two
 # First, find any numbers that are spelled out and convert them. Then rerun part one.
-def PartTwo():
-    pass
+def search_and_replace():
+    with open('input.txt', 'r') as file:
+        file_contents = file.read()
 
+        updated_contents = file_contents.replace('eightwo', '82')
+        updated_contents = updated_contents.replace('oneight', '18')
+        updated_contents = updated_contents.replace('twone', '21')
+        updated_contents = updated_contents.replace('one', '1')
+        updated_contents = updated_contents.replace('two', '2')
+        updated_contents = updated_contents.replace('three', '3')
+        updated_contents = updated_contents.replace('four', '4')
+        updated_contents = updated_contents.replace('five', '5')
+        updated_contents = updated_contents.replace('six', '6')
+        updated_contents = updated_contents.replace('seven', '7')
+        updated_contents = updated_contents.replace('eight', '8')
+        updated_contents = updated_contents.replace('nine', '9')
 
+    with open('new_input.txt', 'w') as file:
+        file.write(updated_contents)
+
+search_and_replace()
+
+with open("new_input.txt", "r") as new_input:
+    edited_data = new_input.read().split('\n')
+
+PartOne(data)
+PartOne(edited_data)
